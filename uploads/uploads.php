ed93a18d4710395a003ea447bc743733e909c00b
Andre Santos <alsantosss@gmail.com>
2020-05-31 15:16:48 -0300

    pequenas alterações no upload

--- uploads/uploads.php
+++ uploads/uploads.php
@@ -8,6 +8,7 @@
 	<body>
 
 		<h1>Formulário de Tratamento de Uploads</h1>
+		<hr>
 		<h3>$_FILES</h3>
 		<p>Array multidimensional composto por <code>$_FILES['chave']['valor'].</code> </p>
 <p>
@@ -19,11 +20,17 @@
 	Chave = size ..... Valor = 997341 .
 	</code>
 </p>
+<hr>
 		<form action="" method="post" enctype="multipart/form-data">
 			
-			
+			<!-- 
+					A Váriável MAX_FILE_SIZE do php pode ser alterada atravez de INPUT 
+			     	e ter seu valor capturado através do $_FILES['error']['2']
+			 -->
+			<input type="hidden" name="MAX_FILE_SIZE" value="10000">
 			<label>Escolha seu arquivo</label>
 			<input type="file" name="arquivo">
+			<p>Tamanho máximo: 10000 kb</p>
 			<br>
 			<br>
 			<input type="submit" name="submit" value="Enviar">
@@ -37,6 +44,10 @@
 
 					$arquivo = $_FILES['arquivo'];
 
+					if ($arquivo['error'] == 2) {
+						echo "<div class=\"message danger\"> Arquivo excede ao tamanho definido! </div>";
+					}
+
 						foreach ($arquivo as $key => $value) {
 							echo "Chave = {$key} e Valor = {$value} . <br>";
 						}
@@ -44,7 +55,7 @@
 					move_uploaded_file($arquivo['tmp_name'], "../assets/img/".$arquivo['name']);
 				}
 		} else {
-			
+
 				echo "Nada foi enviado";
 		}
 		?>
