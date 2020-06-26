bf1c6b5b750af554887664cc6d6bb3bdb94a7509
Andre Santos <alsantosss@hotmail.com>
2020-06-26 14:23:01 -0300

    phpDoc função teste

--- teste.php
+++ teste.php
@@ -1,5 +1,13 @@
 
 <?php
+/**
+ * @author André Santos <alsantosss@gmail.com>
+ * @return $data 
+ * @package Classe parte do projeto
+ * @param mixed $name
+ * 
+ * 
+ */
     $classe = 'classTeste';
     $pastas=['database/',''];
     foreach ($pastas as $pasta) {
